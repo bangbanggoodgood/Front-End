a5ecd5712f449e01dc75b7e58929a10a98a91105
김준하 <junhakjh@gmail.com>
2024-11-20 11:19:02 +0900

    feat: infra, apartment 관련 타입 작성

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,3 +1,5 @@
+import type { infraArray, infraArrayKr } from '@/util/utilKey'
+
 export interface TUser {
   name: string
 }
@@ -10,4 +12,23 @@ export interface TDropDown extends TDropDownList {
   placeholder: string
   isOpened: boolean
   alt: string
+}
+
+export type TInfra = (typeof infraArray)[number]
+export type TInfraKr = (typeof infraArrayKr)[number]
+
+export interface TInfraInfo extends Record<TInfra, number> {}
+
+export interface TApartment {
+  id: number
+  aptNm: string
+  buildYear: string
+  minArea: number
+  maxArea: number
+  address: string
+  infra: TInfraInfo
+  minDealAmount: number
+  maxDealAmount: number
+  introduce: string
+  like: 0 | 1
 }
