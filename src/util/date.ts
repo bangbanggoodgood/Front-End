fa071f4ee75b087e4e4c5ce375f0710ba3e275b0
김준하 <junhakjh@gmail.com>
2024-11-26 10:10:22 +0900

    feat: 생년월일 YY.MM.DD -> YYYYMMDD, 아이디 중복확인 여부 검사

--- src/util/date.ts
+++ src/util/date.ts
@@ -2,14 +2,14 @@ import { daysInMonth } from '@/lib/date'
 
 export const validateBirthDate = (value: string) => {
   // Check if the length is 6
-  if (value.length !== 6) {
+  if (value.length !== 8) {
     return false
   }
 
   // Extract year, month, and day
-  const year = parseInt(value.slice(0, 2), 10)
-  const month = parseInt(value.slice(2, 4), 10)
-  const day = parseInt(value.slice(4, 6), 10)
+  const year = parseInt(value.slice(0, 4), 10)
+  const month = parseInt(value.slice(4, 6), 10)
+  const day = parseInt(value.slice(6, 8), 10)
 
   // Validate month
   if (month < 1 || month > 12) {
@@ -31,13 +31,20 @@ export const validateBirthDate = (value: string) => {
 }
 
 export const formattedDate = (dateString: string): string => {
-  if (dateString.length !== 8 || isNaN(Number(dateString))) {
+  if (isNaN(Number(dateString))) {
     return ''
   }
+  if (dateString.length === 8) {
+    const year = dateString.slice(0, 4)
+    const month = dateString.slice(4, 6)
+    const day = dateString.slice(6, 8)
 
-  const year = dateString.slice(0, 4)
-  const month = dateString.slice(4, 6)
-  const day = dateString.slice(6, 8)
-
-  return `${year}.${month}.${day}`
+    return `${year}.${month}.${day}`
+  }
+  if (dateString.length === 6) {
+    const year = dateString.slice(0, 4)
+    const month = dateString.slice(4, 6)
+    return `${year}.${month}`
+  }
+  return ''
 }
